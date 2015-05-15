bc39a021922a7b087b8cef2a1972aedbbf39b562
david <DAVIDrg?>
2015-05-15 12:28:27 +0200

    Add unit tests

--- app/src/androidTest/java/com/androidxtended/app/FakeThirdPartyServiceTest.java
+++ app/src/androidTest/java/com/androidxtended/app/FakeThirdPartyServiceTest.java
@@ -20,4 +20,43 @@ public class FakeThirdPartyServiceTest {
         // then
         assertEquals("Tu nombre es Pepe y opinas que #AndroidXtended es de lujo", actualMessage);
     }
+
+    @Test
+    public void shouldNoName() {
+        // given
+        String userName = "";
+        String opinion = "de lujo";
+
+        // when
+        String actualMessage = fakeThirdPartyService.buildOpinion(userName, opinion);
+
+        // then
+        assertEquals("No tienes nombre, pero opinas que #AndroidXtended es de lujo", actualMessage);
+    }
+
+    @Test
+    public void shouldNoOpinion() {
+        // given
+        String userName = "Pepe";
+        String opinion = "";
+
+        // when
+        String actualMessage = fakeThirdPartyService.buildOpinion(userName, opinion);
+
+        // then
+        assertEquals("Tu nombre es Pepe, pero no opinas", actualMessage);
+    }
+
+    @Test
+    public void shouldNothing() {
+        // given
+        String userName = "";
+        String opinion = "";
+
+        // when
+        String actualMessage = fakeThirdPartyService.buildOpinion(userName, opinion);
+
+        // then
+        assertEquals("Ni tienes nombre ni opinas", actualMessage);
+    }
 }
--- app/src/main/java/com/androidxtended/app/FakeThirdPartyService.java
+++ app/src/main/java/com/androidxtended/app/FakeThirdPartyService.java
@@ -3,11 +3,15 @@ package com.androidxtended.app;
 public class FakeThirdPartyService {
 
     public String buildOpinion(final String userName, final String opinion) {
-        String opinionFromServer = getOpinionFromServer();
-        return String.format(opinionFromServer, userName, opinion);
-    }
 
-    private String getOpinionFromServer() {
-        return "Tu nombre es %s y opinas que #AndroidXtended es %s";
+        if (opinion.isEmpty() && userName.isEmpty()) {
+            return "Ni tienes nombre ni opinas";
+        } else if (opinion.isEmpty()) {
+            return String.format("Tu nombre es %s, pero no opinas", userName);
+        } else if (userName.isEmpty()) {
+            return String.format("No tienes nombre, pero opinas que #AndroidXtended es %s", opinion);
+        } else {
+            return String.format("Tu nombre es %s y opinas que #AndroidXtended es %s", userName, opinion);
+        }
     }
 }
